c8a520137e9cf861248d878716b9ada3a365936f
Michael Greenshtein <michaelg@Michael-Gs-MacBook-Pro.local>
2018-01-10 16:52:02 +0200

    .yml changed

--- test/test.js
+++ test/test.js
@@ -10,13 +10,18 @@ describe('Array', function() {
 
 describe('Environement settings', function() {
   describe('#Environment variables are set', function() {
+    
     it('it should return env variable TEST=yes', function() {
       assert.equal(process.env.TEST, "yes");
     });
     
-    // it('it should check CLOUDINARY_URL', function() {
-    //   assert.equal(process.env.CLOUDINARY_URL, "cloudinary://09287340928734:638nxwbfFZQk@sdk-test");
-    // });
+    it('it should check CLOUDINARY_URL', function() {
+      assert.equal(process.env.CLOUDINARY_URL, "staging");
+    });
+    
+    it('it should check TRAVIS_BUILD', function() {
+      assert.ok(process.env.TRAVIS_BUILD.indexOf("jenkins-staging")>-1);
+    });
   });
 });
 
